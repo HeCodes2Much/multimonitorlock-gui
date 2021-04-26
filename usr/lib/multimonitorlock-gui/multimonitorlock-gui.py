c40c4381c97a2f68ac424eaa7a8914ac8bef8d87
The-Repo-Club <wayne6324@gmail.com>
2021-04-26 18:16:43 +0100

    Update (Added a splash screen)

--- usr/lib/multimonitorlock-gui/multimonitorlock-gui.py
+++ usr/lib/multimonitorlock-gui/multimonitorlock-gui.py
@@ -10,7 +10,9 @@ import os
 import re
 import shutil
 import subprocess
+import platform
 import Functions as fn
+from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtGui import *
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
@@ -20,6 +22,12 @@ def divide_chunks(l, n):
     # looping till length l
     for i in range(0, len(l), n):
         yield l[i:i + n]
+
+## ==> SPLASH SCREEN
+from ui_splash_screen import Ui_SplashScreen
+
+## ==> GLOBALS
+counter = 0
 
 class MultiMonitorLock(QWidget):
     EXIT_CODE_REBOOT = -15123123
@@ -78,8 +86,9 @@ class MultiMonitorLock(QWidget):
             val = 0
             for image in imagearray:
                 val = val + 1
-                self.image_select = QToolButton()
-                self.image_select.setIcon(QIcon(self.directory + "/" + image))
+                pixmap = QPixmap(self.directory + "/" + image)
+                self.image_select = QPushButton()
+                self.image_select.setIcon(pixmap)
                 self.image_select.setIconSize(QSize(150, 150))
                 self.image_select.setCheckable(True);
                 self.image_select.setObjectName(self.directory + "/" + image)
@@ -246,6 +255,73 @@ class MultiMonitorLock(QWidget):
             else:
                 pass
 
+# SPLASH SCREEN
+class SplashScreen(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_SplashScreen()
+        self.ui.setupUi(self)
+
+        ## UI ==> INTERFACE CODES
+        ########################################################################
+
+        ## REMOVE TITLE BAR
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+
+        ## DROP SHADOW EFFECT
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 60))
+        self.ui.dropShadowFrame.setGraphicsEffect(self.shadow)
+
+        ## QTIMER ==> START
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.progress)
+        # TIMER IN MILLISECONDS
+        self.timer.start(35)
+
+        # CHANGE DESCRIPTION
+
+        # Initial Text
+        self.ui.label_description.setText("<strong>WELCOME</strong> TO MULTIMONITORLOCK-GUI")
+
+        # Change Texts
+        QtCore.QTimer.singleShot(2000, lambda: self.ui.label_description.setText("<strong>LOADING</strong> USER INTERFACE"))
+
+
+        ## SHOW ==> MAIN WINDOW
+        ########################################################################
+        self.show()
+        ## ==> END ##
+
+    ## ==> APP FUNCTIONS
+    ########################################################################
+    def progress(self):
+
+        global counter
+
+        # SET VALUE TO PROGRESS BAR
+        self.ui.progressBar.setValue(counter)
+
+        # CLOSE SPLASH SCREE AND OPEN APP
+        if counter > 100:
+            # STOP TIMER
+            self.timer.stop()
+
+            # SHOW MAIN WINDOW
+            self.main = MultiMonitorLock()
+            self.main.show()
+
+            # CLOSE SPLASH SCREEN
+            self.close()
+
+        # INCREASE COUNTER
+        counter += 1
+
 
 if __name__ == '__main__':
     currentExitCode = MultiMonitorLock.EXIT_CODE_REBOOT
@@ -257,7 +333,7 @@ if __name__ == '__main__':
         QCoreApplication.setOrganizationName("The-Repo-Club")
         QCoreApplication.setOrganizationDomain("github.com/The-Repo-Club")
         QCoreApplication.setApplicationName("MultiMonitorLock-Gui")
-        Gui = MultiMonitorLock()
+        Gui = SplashScreen()
         Gui.show()
         currentExitCode = app.exec_()
         app = None
